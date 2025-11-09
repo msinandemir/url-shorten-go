8c66b7c87cf79c0100a57a5a3296ff3f5f500c28
Sinan <sinandeemir@gmail.com>
2025-11-09 16:50:06 +0300

    refactor(api.go): Gin serverını api.go dosyasında ayağa kaldır.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,25 @@
 package api
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"sdemir.com/configurations"
+	"sdemir.com/interval/handlers"
+)
+
+func NewApi(urlHandler handlers.UrlHandler) {
+	APP_PORT := fmt.Sprintf(":%d", configurations.GetEnv.APP_PORT)
+	app := gin.Default()
+	setupUrlRoutes(app, urlHandler)
+
+	if err := app.Run(APP_PORT); err != nil {
+		fmt.Printf("Server başlatılırken hata oluştu. Error: %s", err)
+	}
+}
+
+func setupUrlRoutes(engine *gin.Engine, urlHandler handlers.UrlHandler) *gin.Engine {
+	engine.POST("/shorten", urlHandler.CreateShortenUrl)
+	engine.GET("/:short-url", urlHandler.RedirectToUrl)
+	return engine
+}
