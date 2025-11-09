f282f490fa94a716d5846d47ca235c68946ae996
Sinan <sinandeemir@gmail.com>
2025-11-09 16:45:53 +0300

    feat(utils): kısa urlleri oluşturmak için yardımcı sınıf oluştur.

---
+++ utils/shortner.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func GenerateShortCode(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
